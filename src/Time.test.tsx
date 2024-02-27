754aff30008d32f137039480dec578d4d9be1844
dmitriiTr <56300102+dmitriiTr@users.noreply.github.com>
2024-02-27 16:26:00 +0300

    Tests work with react scripts

---
+++ src/Time.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import { Time } from './Time';
+
+describe('TEST APP Time', () => {
+  test('renders learn react link', () => {
+    render(<Time hours={0} minutes={0} seconds={0}/>);
+    const helloWorldElem = screen.getByText(/0/i);
+    expect(helloWorldElem).toBeInTheDocument();
+  });
+});
