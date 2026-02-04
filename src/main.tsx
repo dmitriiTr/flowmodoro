b9bd9823f3975169ebad4482f28d1f12258691bf
dmitriiTr <56300102+dmitriiTr@users.noreply.github.com>
2026-02-04 09:49:23 +0300

    css file removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import './index.css';
-
 import App from './App.tsx';
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { TasksContextProvider } from './TasksContextProvider.tsx';
@@ -8,6 +7,7 @@ import { TasksContextProvider } from './TasksContextProvider.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TasksContextProvider>
+      <CssBaseline />
       <App />
     </TasksContextProvider>
   </React.StrictMode>
